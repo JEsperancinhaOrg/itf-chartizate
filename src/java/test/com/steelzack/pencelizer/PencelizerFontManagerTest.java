b824c614bdb3bbb0a4eaf3b33ba556dce6138f74
Joao Esperancinha <jofisaes@hotmail.com>
2016-01-24 22:48:00 +0100

    SZ-N Adds font manager

---
+++ src/java/test/com/steelzack/pencelizer/PencelizerFontManagerTest.java
@@ -0,0 +1,22 @@
+package com.steelzack.pencelizer;
+
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+
+public class PencelizerFontManagerTest {
+
+	@Test
+	public void testPencelizerFontManager() throws Exception {
+		throw new RuntimeException("not yet implemented");
+	}
+
+	@Test
+	public void testGetCharacterWidth() throws Exception {
+		final PencelizerFontManager manager = new PencelizerFontManager("Arial", 10);
+		
+		assertEquals(6, manager.getCharacterWidth('a'));
+		assertEquals(7, manager.getCharacterWidth('A'));
+		
+	}
+}
