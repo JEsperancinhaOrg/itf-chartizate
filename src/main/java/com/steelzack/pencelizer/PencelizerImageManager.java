655ebefe3de956774a969d10d2131a7f2eecb0e6
Joao Esperancinha <jofisaes@hotmail.com>
2016-01-31 13:32:47 +0100

    SZ-N Calculates average by channels
    NOTE: previous average calculation was not correct. sRGB color is just a number representation of bit sequence that contains information about all four channels.
    An average of this number yealds dus unpredictable results

--- src/main/java/com/steelzack/pencelizer/PencelizerImageManager.java
+++ src/main/java/com/steelzack/pencelizer/PencelizerImageManager.java
@@ -2,6 +2,7 @@ package com.steelzack.pencelizer;
 
 import static java.lang.Math.abs;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
@@ -22,17 +23,34 @@ public class PencelizerImageManager {
 	}
 
 	public long getImageAverageColor() {
-		final int width = srcImage.getWidth();
-		final int height = srcImage.getHeight();
+		final int width = srcImage.getWidth() - 1;
+		final int height = srcImage.getHeight() - 1;
+		return getPartAverageColor(0, 0, width, height);
+	}
 
-		double fullSum = 0d;
+	public int getPartAverageColor(final int x0, final int y0, final int xn, final int yn) {
+		double alpha = 0;
+		double red = 0;
+		double green = 0;
+		double blue = 0;
+		for (int j = x0; j <= xn; j++) {
+			for (int k = y0; k <= yn; k++) {
+				int rgbPixel = srcImage.getRGB(j, k);
+				alpha += (rgbPixel >> 24) & 0xff;
+				red += (rgbPixel >> 16) & 0xff;
+				green += (rgbPixel >> 8) & 0xff;
+				blue += (rgbPixel) & 0xff;
 
-		for (int j = 0; j < width; j++) {
-			for (int k = 0; k < height; k++) {
-				fullSum += srcImage.getRGB(j, k);
 			}
 		}
-
-		return abs((long) (fullSum / (width * height)));
+		
+		int commonDenominator = (xn - x0 + 1) * (yn - y0 + 1);
+		int mediumApha = (int) (alpha / commonDenominator);
+		int mediumRed = (int) (red / commonDenominator);
+		int mediumBlue = (int) (blue / commonDenominator);
+		int mediumGreen = (int) (green / commonDenominator);
+		
+		int fullSum = new Color(mediumRed, mediumGreen, mediumBlue, mediumApha).getRGB();
+		return fullSum;
 	}
 }
