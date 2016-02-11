5a341a3662dc1eeab5533e0127ab5bb7b11ae31d
Joao Esperancinha <jofisaes@hotmail.com>
2016-02-11 23:31:21 +0100

    SZ-N Adds unit tests

---
+++ chartizate-text-writer-android/src/androidTest/java/com/steelzack/chartizate/ChartizateFontManagerImplTest.java
@@ -0,0 +1,29 @@
+package com.steelzack.chartizate;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by joaofilipesabinoesperancinha on 11-02-16.
+ */
+public class ChartizateFontManagerImplTest extends TestCase {
+
+    public void testGetAllUniCodeBlockStringsJava7() throws Exception {
+
+    }
+
+    public void testGetAllUniCodeBlockStringsJava8() throws Exception {
+
+    }
+
+    public void testGetCharacterWidth() throws Exception {
+
+    }
+
+    public void testGetCharacterHeight() throws Exception {
+
+    }
+
+    public void testGetFont() throws Exception {
+
+    }
+}
---
+++ chartizate-text-writer-android/src/androidTest/java/com/steelzack/chartizate/ChartizateManagerImplTest.java
@@ -0,0 +1,29 @@
+package com.steelzack.chartizate;
+
+import junit.framework.TestCase;
+
+/**
+ * Created by joaofilipesabinoesperancinha on 11-02-16.
+ */
+public class ChartizateManagerImplTest extends TestCase {
+
+    public void testCreateImageManager() throws Exception {
+
+    }
+
+    public void testCreateEncodingManager() throws Exception {
+
+    }
+
+    public void testCreateFontManager() throws Exception {
+
+    }
+
+    public void testAddFullRow() throws Exception {
+
+    }
+
+    public void testGenerateConvertedImage() throws Exception {
+
+    }
+}
