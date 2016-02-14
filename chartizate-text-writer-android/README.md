fdf08729c3c5300e51ac93baee25f0b2b150c303
Joao F.S.E <jofisaes@hotmail.com>
2016-02-14 17:22:38 +0100

    SZ-N Completes release 1.0.0 in Bintray / JFrog

---
+++ chartizate-text-writer-android/README.md
@@ -0,0 +1,35 @@
+# chartizate-text-writer-android
+
+[ ![Download](https://api.bintray.com/packages/jesperancinha/maven/chartizate-text-writer-android/images/download.svg) ](https://bintray.com/jesperancinha/maven/chartizate-text-writer-android/_latestVersion)
+
+## Description
+
+The aim of this library is to transform an image into a character filled data that still represents the original image
+
+From
+
+![realexample1.png](https://raw.githubusercontent.com/jesperancinha/lib-chartizate-sz/master/chartizate-text-writer-android/src/main/res/raw/realexample1.jpg)
+
+To
+
+![realexample1expected.png](https://raw.githubusercontent.com/jesperancinha/lib-chartizate-sz/master/chartizate-text-writer-android/src/main/res/raw/realexample1expected.png)
+
+
+## License
+
+```
+Copyright 2016-2017 Steelzack
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+```
+
--- chartizate-text-writer-api/src/main/java/com/steelzack/chartizate/ChartizateFontManager.java
+++ chartizate-text-writer-api/src/main/java/com/steelzack/chartizate/ChartizateFontManager.java
@@ -1,13 +1,238 @@
 package com.steelzack.chartizate;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
-public interface ChartizateFontManager<T> {
+import com.steelzack.chartizate.distributions.ChartizateDistributionType;
 
-	List<String> getAllUniCodeBlockStringsJava7();
+public abstract class  ChartizateFontManager<T> {
 
-	List<String> getAllUniCodeBlockStringsJava8();
+	static final String[] unicodeDesignationsJava7 = new String[] { //
+			"BASIC_LATIN", //
+			"LATIN_1_SUPPLEMENT", //
+			"LATIN_EXTENDED_A", //
+			"LATIN_EXTENDED_B", //
+			"IPA_EXTENSIONS", //
+			"SPACING_MODIFIER_LETTERS", //
+			"COMBINING_DIACRITICAL_MARKS", //
+			"GREEK", //
+			"CYRILLIC", //
+			"ARMENIAN", //
+			"HEBREW", //
+			"ARABIC", //
+			"DEVANAGARI", //
+			"BENGALI", //
+			"GURMUKHI", //
+			"GUJARATI", //
+			"ORIYA", //
+			"TAMIL", //
+			"TELUGU", //
+			"KANNADA", //
+			"MALAYALAM", //
+			"THAI", //
+			"LAO", //
+			"TIBETAN", //
+			"GEORGIAN", //
+			"HANGUL_JAMO", //
+			"LATIN_EXTENDED_ADDITIONAL", //
+			"GREEK_EXTENDED", //
+			"GENERAL_PUNCTUATION", //
+			"SUPERSCRIPTS_AND_SUBSCRIPTS", //
+			"CURRENCY_SYMBOLS", //
+			"COMBINING_MARKS_FOR_SYMBOLS", //
+			"LETTERLIKE_SYMBOLS", //
+			"NUMBER_FORMS", //
+			"ARROWS", //
+			"MATHEMATICAL_OPERATORS", //
+			"MISCELLANEOUS_TECHNICAL", //
+			"CONTROL_PICTURES", //
+			"OPTICAL_CHARACTER_RECOGNITION", //
+			"ENCLOSED_ALPHANUMERICS", //
+			"BOX_DRAWING", //
+			"BLOCK_ELEMENTS", //
+			"GEOMETRIC_SHAPES", //
+			"MISCELLANEOUS_SYMBOLS", //
+			"DINGBATS", //
+			"CJK_SYMBOLS_AND_PUNCTUATION", //
+			"HIRAGANA", //
+			"KATAKANA", //
+			"BOPOMOFO", //
+			"HANGUL_COMPATIBILITY_JAMO", //
+			"KANBUN", //
+			"ENCLOSED_CJK_LETTERS_AND_MONTHS", //
+			"CJK_COMPATIBILITY", //
+			"CJK_UNIFIED_IDEOGRAPHS", //
+			"HANGUL_SYLLABLES", //
+			"PRIVATE_USE_AREA", //
+			"CJK_COMPATIBILITY_IDEOGRAPHS", //
+			"ALPHABETIC_PRESENTATION_FORMS", //
+			"ARABIC_PRESENTATION_FORMS_A", //
+			"COMBINING_HALF_MARKS", //
+			"CJK_COMPATIBILITY_FORMS", //
+			"SMALL_FORM_VARIANTS", //
+			"ARABIC_PRESENTATION_FORMS_B", //
+			"HALFWIDTH_AND_FULLWIDTH_FORMS", //
+			"SPECIALS", //
+			"SYRIAC", //
+			"THAANA", //
+			"SINHALA", //
+			"MYANMAR", //
+			"ETHIOPIC", //
+			"CHEROKEE", //
+			"UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS", //
+			"OGHAM", //
+			"RUNIC", //
+			"KHMER", //
+			"MONGOLIAN", //
+			"BRAILLE_PATTERNS", //
+			"CJK_RADICALS_SUPPLEMENT", //
+			"KANGXI_RADICALS", //
+			"IDEOGRAPHIC_DESCRIPTION_CHARACTERS", //
+			"BOPOMOFO_EXTENDED", //
+			"CJK_UNIFIED_IDEOGRAPHS_EXTENSION_A", //
+			"YI_SYLLABLES", //
+			"YI_RADICALS", //
+			"CYRILLIC_SUPPLEMENTARY", //
+			"TAGALOG", //
+			"HANUNOO", //
+			"BUHID", //
+			"TAGBANWA", //
+			"LIMBU", //
+			"TAI_LE", //
+			"KHMER_SYMBOLS", //
+			"PHONETIC_EXTENSIONS", //
+			"MISCELLANEOUS_MATHEMATICAL_SYMBOLS_A", //
+			"SUPPLEMENTAL_ARROWS_A", //
+			"SUPPLEMENTAL_ARROWS_B", //
+			"MISCELLANEOUS_MATHEMATICAL_SYMBOLS_B", //
+			"SUPPLEMENTAL_MATHEMATICAL_OPERATORS", //
+			"MISCELLANEOUS_SYMBOLS_AND_ARROWS", //
+			"KATAKANA_PHONETIC_EXTENSIONS", //
+			"YIJING_HEXAGRAM_SYMBOLS", //
+			"VARIATION_SELECTORS", //
+			"LINEAR_B_SYLLABARY", //
+			"LINEAR_B_IDEOGRAMS", //
+			"AEGEAN_NUMBERS", //
+			"OLD_ITALIC", //
+			"GOTHIC", //
+			"UGARITIC", //
+			"DESERET", //
+			"SHAVIAN", //
+			"OSMANYA", //
+			"CYPRIOT_SYLLABARY", //
+			"BYZANTINE_MUSICAL_SYMBOLS", //
+			"MUSICAL_SYMBOLS", //
+			"TAI_XUAN_JING_SYMBOLS", //
+			"MATHEMATICAL_ALPHANUMERIC_SYMBOLS", //
+			"CJK_UNIFIED_IDEOGRAPHS_EXTENSION_B", //
+			"CJK_COMPATIBILITY_IDEOGRAPHS_SUPPLEMENT", //
+			"TAGS", //
+			"VARIATION_SELECTORS_SUPPLEMENT", //
+			"SUPPLEMENTARY_PRIVATE_USE_AREA_A", //
+			"SUPPLEMENTARY_PRIVATE_USE_AREA_B", //
+			"HIGH_SURROGATES", //
+			"HIGH_PRIVATE_USE_SURROGATES", //
+			"LOW_SURROGATES", //
+			"ARABIC_SUPPLEMENT", //
+			"NKO", //
+			"SAMARITAN", //
+			"MANDAIC", //
+			"ETHIOPIC_SUPPLEMENT", //
+			"UNIFIED_CANADIAN_ABORIGINAL_SYLLABICS_EXTENDED", //
+			"NEW_TAI_LUE", //
+			"BUGINESE", //
+			"TAI_THAM", //
+			"BALINESE", //
+			"SUNDANESE", //
+			"BATAK", //
+			"LEPCHA", //
+			"OL_CHIKI", //
+			"VEDIC_EXTENSIONS", //
+			"PHONETIC_EXTENSIONS_SUPPLEMENT", //
+			"COMBINING_DIACRITICAL_MARKS_SUPPLEMENT", //
+			"GLAGOLITIC", //
+			"LATIN_EXTENDED_C", //
+			"COPTIC", //
+			"GEORGIAN_SUPPLEMENT", //
+			"TIFINAGH", //
+			"ETHIOPIC_EXTENDED", //
+			"CYRILLIC_EXTENDED_A", //
+			"SUPPLEMENTAL_PUNCTUATION", //
+			"CJK_STROKES", //
+			"LISU", //
+			"VAI", //
+			"CYRILLIC_EXTENDED_B", //
+			"BAMUM", //
+			"MODIFIER_TONE_LETTERS", //
+			"LATIN_EXTENDED_D", //
+			"SYLOTI_NAGRI", //
+			"COMMON_INDIC_NUMBER_FORMS", //
+			"PHAGS_PA", //
+			"SAURASHTRA", //
+			"DEVANAGARI_EXTENDED", //
+			"KAYAH_LI", //
+			"REJANG", //
+			"HANGUL_JAMO_EXTENDED_A", //
+			"JAVANESE", //
+			"CHAM", //
+			"MYANMAR_EXTENDED_A", //
+			"TAI_VIET", //
+			"ETHIOPIC_EXTENDED_A", //
+			"MEETEI_MAYEK", //
+			"HANGUL_JAMO_EXTENDED_B", //
+			"VERTICAL_FORMS", //
+			"ANCIENT_GREEK_NUMBERS", //
+			"ANCIENT_SYMBOLS", //
+			"PHAISTOS_DISC", //
+			"LYCIAN", //
+			"CARIAN", //
+			"OLD_PERSIAN", //
+			"IMPERIAL_ARAMAIC", //
+			"PHOENICIAN", //
+			"LYDIAN", //
+			"KHAROSHTHI", //
+			"OLD_SOUTH_ARABIAN", //
+			"AVESTAN", //
+			"INSCRIPTIONAL_PARTHIAN", //
+			"INSCRIPTIONAL_PAHLAVI", //
+			"OLD_TURKIC", //
+			"RUMI_NUMERAL_SYMBOLS", //
+			"BRAHMI", //
+			"KAITHI", //
+			"CUNEIFORM", //
+			"CUNEIFORM_NUMBERS_AND_PUNCTUATION", //
+			"EGYPTIAN_HIEROGLYPHS", //
+			"BAMUM_SUPPLEMENT", //
+			"KANA_SUPPLEMENT", //
+			"ANCIENT_GREEK_MUSICAL_NOTATION", //
+			"COUNTING_ROD_NUMERALS", //
+			"MAHJONG_TILES", //
+			"DOMINO_TILES", //
+			"PLAYING_CARDS", //
+			"ENCLOSED_ALPHANUMERIC_SUPPLEMENT", //
+			"ENCLOSED_IDEOGRAPHIC_SUPPLEMENT", //
+			"MISCELLANEOUS_SYMBOLS_AND_PICTOGRAPHS", //
+			"EMOTICONS", //
+			"TRANSPORT_AND_MAP_SYMBOLS", //
+			"ALCHEMICAL_SYMBOLS", //
+			"CJK_UNIFIED_IDEOGRAPHS_EXTENSION_C", //
+			"CJK_UNIFIED_IDEOGRAPHS_EXTENSION_D", //
+	};
+	
 
+	public static List<String> getAllUniCodeBlockStringsJava7() {
+		return Arrays.asList(unicodeDesignationsJava7);
+	}
+
+	public static List<String> getAllDistributionTypes() {
+		final List<String> distributionTypes = new ArrayList<>();
+
+		for (ChartizateDistributionType type : Arrays.asList(ChartizateDistributionType.values())) {
+			distributionTypes.add(type.toString());
+		}
+		return distributionTypes;
+	}
 	/**
 	 * Gets the width of a particular character
 	 * 
@@ -15,7 +240,7 @@ public interface ChartizateFontManager<T> {
 	 *            Character
 	 * @return Width of character
 	 */
-	int getCharacterWidth(char c);
+	abstract int getCharacterWidth(char c);
 
 	/**
 	 * Gets full height of a particular character
@@ -24,9 +249,9 @@ public interface ChartizateFontManager<T> {
 	 *            Character
 	 * @return Width of character
 	 */
-	int getCharacterHeight(char c);
+	abstract int getCharacterHeight(char c);
 
-	T getFont();
+	abstract T getFont();
 	
-	int getFontSize();
+	abstract int getFontSize();
 }
